c2cfa20bfa6bd73ef6804f2fa6ae8741db5baa1a
Hidde-Jan Jongsma <hidde-jan@vergil.nl>
2012-05-17 11:11:00 +0200

    Dutch translations

---
+++ i18n/jquery.multiselect.filter.nl.js
@@ -0,0 +1,11 @@
+/* Dutch initialisation for the jQuery UI multiselect plugin. */
+/* Written by Hidde-Jan Jongsma (hidde-jan@vergil.nl). */
+
+(function ( $ ) {
+
+$.extend($.ech.multiselectfilter.prototype.options, {
+	label: "Zoek:",
+	placeholder: "Geef zoektermen op"
+});
+
+})( jQuery );
