davidworkman9/jquery-ui-multiselect-widget

Update src/jquery.multiselect.js
added touchstart event to close the multiselect on mobile versions of the safari.

src/jquery.multiselect.js
@@ -399,7 +399,7 @@
       });
 
       // close each widget when clicking on any other element/anywhere else on the page
-      $doc.bind('mousedown.' + this._namespaceID, function(e) {
+      $doc.bind('mousedown.' + this._namespaceID ' touchstart' + this._namespaceID, function(e) {
         if(self._isOpen && !$.contains(self.menu[0], e.target) && !$.contains(self.button[0], e.target) && e.target !== self.button[0]) {
           self.close();
         }

GerHobbelt/jquery-multiselect

src/jquery.multiselect.js
@@ -442,7 +442,7 @@
       });
 
       // close each widget when clicking on any other element/anywhere else on the page
-      $doc.bind('mousedown.' + this._namespaceID, function (event) {
+      $doc.bind('mousedown.' + this._namespaceID ' touchstart' + this._namespaceID, function(event) {
         var target = event.target;
 
         if (self._isOpen